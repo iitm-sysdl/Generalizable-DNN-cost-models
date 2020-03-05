65725998565a66511ba4788e613288d6427e7b42
surya <surya00060@gmail.com>
2020-03-05 15:44:35 +0530

    Added PyTorch Regression Notebook

---
+++ simpleconvnetgenerator/data+model/modelpytorch.py
@@ -0,0 +1,14 @@
+import numpy as np
+from matplotlib import pyplot as plt 
+from numpy import genfromtxt
+import csv
+import torch
+import torchvision
+import torch.nn as nn
+
+
+
+
+
+model=nn.Sequential()
+loss=nn.MS
