1ae2f8165f8acd047b822ff140f0c7e7fd1278b9
Vinod Ganesan <g.vinod1993@gmail.com>
2020-04-24 00:08:03 +0530

    Bug client

--- client-server/client.py
+++ client-server/client.py
@@ -7,15 +7,21 @@ import os
 SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096 # send 4096 bytes each time step
 
-host = "10.243.69.84"
+host = "192.168.1.24"
 # the port, let's use 5001
-port = 8006
+port = 5001
 # the name of file we want to send, make sure it exists
 filename = "netFeatures.csv"
 # get the file size
 filesize = os.path.getsize(filename)
 
 s = socket.socket()
+
+print(f"[+] Connecting to {host}:{port}")
+s.connect((host, port))
+print("[+] Connected.")
+
+s.send(f"{filename}{SEPARATOR}{filesize}".encode())
 
 
 progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
