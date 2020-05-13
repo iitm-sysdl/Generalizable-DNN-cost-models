d8184f61c67eb2ad1866d45435b502004c9bb2da
surya00060 <surya00060@gmail.com>
2020-05-13 22:45:54 +0530

    Added Width Multiplier, Added Small, Large, Giant Models

--- MobileTargetedGenerator/targertedGen.py
+++ MobileTargetedGenerator/targertedGen.py
@@ -9,21 +9,35 @@ from random import sample
 from random import randrange
 from torchprofile import profile_macs
 from matplotlib import pyplot as plt
-import tensorflow as tf
-import onnx
-import torch.onnx
-import onnx2keras
-from onnx2keras import onnx_to_keras
+# import tensorflow as tf
+# import onnx
+# import torch.onnx
+# import onnx2keras
+# from onnx2keras import onnx_to_keras
 
-numSamples = 10
+numSamples = 25
 
-kernelList = [3, 5, 7]
+modelSize = ['small', 'large']
+widthMultiplier = [0.75, 1]
+kernelList = {'small':[3, 5], 
+              'large':[3, 5, 7],
+              'giant': [3,5,7]
+             }
+
 expansionList = [3, 6]
-paddingDict ={1:0, 3:1, 5:2, 7:3}
-channelListPerDim = {112:[16, 24, 32], 56:[24, 32, 40], 28:[80, 88, 96, 108], 14:[192, 216, 240, 320], 7:[960, 1024, 1280]}
-numMBsPerDim = {112:[2,3], 56:[3,4,5], 28:[5,6,7,8,9], 14:[3,4,5]}
-file = open("Embeddings.csv", 'w')
+paddingDict = {1:0, 3:1, 5:2, 7:3}
 
+channelListPerDim = { 'small': {56:[ 16, 24 ], 28:[ 36, 40, 44, 48], 14:[80, 88, 96], 7:[576, 584], 1:[1024, 1064]}, 
+                      'large': {112:[16, 24],  56:[ 32, 40, 48], 28:[80, 88, 96, 104, 112], 14:[160, 168, 176], 7:[920, 960], 1:[1280, 1320]},
+                      'giant': {112:[16, 24, 32], 56:[24, 32, 40], 28:[80, 88, 96, 108], 14:[192, 216, 240, 320], 7:[960, 968], 1:[1280, 1320]}
+                    }
+
+numMBsPerDim = {'small': { 56:[2,3], 28:[3,4,5], 14:[3,4] },
+                'large': {112:[2,3], 56:[3,4],   28:[5,6,7,8], 14:[3,4,5]},
+                'giant': {112:[2,3], 56:[3,4,5], 28:[5,6,7,8,9], 14:[3,4,5]}
+               }
+
+file = open("Embeddings.csv", 'w')
 class Conv2d(nn.Module):
     def __init__(self, in_planes, out_planes, kernel, stride, padding):
         super(Conv2d, self).__init__()
@@ -33,11 +47,13 @@ class Conv2d(nn.Module):
         return x
 
 class MobileBottleNeck(nn.Module):
-    def __init__(self, inplanes, outplanes, expansion, kernel, stride, padding, skip):
+    def __init__(self, inplanes, outplanes, expansion, kernel, stride, padding, skip, width):
         super(MobileBottleNeck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, inplanes*expansion, kernel_size=1, bias=False)
-        self.conv2 = nn.Conv2d(inplanes*expansion, inplanes*expansion, kernel_size=kernel, stride=stride, padding=padding, bias=False, groups=inplanes*expansion)
-        self.conv3 = nn.Conv2d(inplanes*expansion, outplanes, kernel_size=1, bias=False)
+        expandChannels = int(inplanes*expansion*width)
+        outChannels = int(outplanes*width)
+        self.conv1 = nn.Conv2d(inplanes, expandChannels, kernel_size=1, bias=False)
+        self.conv2 = nn.Conv2d(expandChannels, expandChannels, kernel_size=kernel, stride=stride, padding=padding, bias=False, groups=expandChannels)
+        self.conv3 = nn.Conv2d(expandChannels, outChannels, kernel_size=1, bias=False)
         self.skip = skip
     def forward(self, x):
         out = self.conv1(x)
@@ -59,22 +75,25 @@ def depthConv(inDim, inC, kernel, stride, padding, netEmbedding):
     inDim = inDim/stride
     return inDim
 
-def mobileBottleneckConv(inDim, inC, outC, expansion, kernel, stride, padding, skip, netEmbedding):
-    inDim = convolution(inDim, inC, expansion*inC, 1, 1, 0, netEmbedding)
+def mobileBottleneckConv(inDim, inC, outC, expansion, kernel, stride, padding, skip, width, netEmbedding):
+    expandChannels = int(inC*expansion*width)
+    outChannels = int(outC*width)
+
+    inDim = convolution(inDim, inC, expandChannels, 1, 1, 0, netEmbedding)
     
     #ReLU Embedding
-    netEmbedding.append([0,0,0,1,0, inDim, inDim, expansion*inC, expansion*inC, 0, 0, 0, inDim*inDim*inC*expansion])
+    netEmbedding.append([0,0,0,1,0, inDim, inDim, expandChannels, expandChannels, 0, 0, 0, inDim*inDim*expandChannels])
     
-    inDim = depthConv(inDim, inC*expansion, kernel, stride, padding, netEmbedding)
+    inDim = depthConv(inDim, expandChannels, kernel, stride, padding, netEmbedding)
     
     #ReLU Embedding
-    netEmbedding.append([0,0,0,1,0, inDim, inDim, expansion*inC, expansion*inC, 0, 0, 0, inDim*inDim*inC*expansion])
+    netEmbedding.append([0,0,0,1,0, inDim, inDim, expandChannels, expandChannels, 0, 0, 0, inDim*inDim*expandChannels])
     
-    inDim = convolution(inDim, inC*expansion, outC, 1, 1, 0, netEmbedding)
+    inDim = convolution(inDim, expandChannels, outChannels, 1, 1, 0, netEmbedding)
     
     if skip == True:
-        netEmbedding.append([0,0,1,0,0, inDim, inDim, outC, outC, 0, 0, 0, inDim*inDim*outC])
-    return inDim
+        netEmbedding.append([0,0,1,0,0, inDim, inDim, outChannels, outChannels, 0, 0, 0, inDim*inDim*outChannels])
+    return inDim, outChannels
 
 def pooling(inDim, kernel, channels, netEmbedding):
     netEmbedding.append([0,0,0,0,1, inDim, inDim/kernel, channels, channels, 0, 0, 0, (inDim/kernel)*(inDim/kernel)*kernel*kernel*outC])
@@ -85,6 +104,9 @@ flopsList = []
 modelSizeList = []
 inferenceTimeList = []
 for i in range(numSamples):
+    # size = random.choice(modelSize)
+    size ='small'
+    width = random.choice(widthMultiplier)
     netEmbedding = []
     network = []
     numSkip = 0
@@ -100,33 +122,33 @@ for i in range(numSamples):
     
     inC = outC
     k = 3
-    s = 1
+    s = 2 if size == 'small' else 1
     outC = 16
     e = 1
     skip = False
-    network.append(MobileBottleNeck(inC, outC, e, k, s, paddingDict[k], skip))
-    inDim = mobileBottleneckConv(inDim, inC, outC, e, k, s, paddingDict[k], skip, netEmbedding)
+    network.append(MobileBottleNeck(inC, outC, e, k, s, paddingDict[k], skip, width))
+    inDim, outC = mobileBottleneckConv(inDim, inC, outC, e, k, s, paddingDict[k], skip, width, netEmbedding)
     numMB+=1
     ## 16 x 112 x 112
     while inDim != 7:
-        numConvList = numMBsPerDim[inDim]
-        outChannelList = channelListPerDim[inDim]
+        numConvList = numMBsPerDim[size][inDim]
+        outChannelList = channelListPerDim[size][inDim]
         numConv = random.choice(numConvList)
         for j in range(numConv):
             inC= outC
             outC = random.choice(outChannelList)
-            k = random.choice(kernelList)
+            k = random.choice(kernelList[size])
             s = 2 if j == 0 else 1
             e = random.choice(expansionList)
-            skip = True if inC == outC and s == 1 else False
+            skip = True if inC == int(outC*width) and s == 1 else False
             if skip == True:
                 numSkip += 1
-            network.append(MobileBottleNeck(inC, outC, e, k, s, paddingDict[k], skip))
-            inDim = mobileBottleneckConv(inDim, inC, outC, e, k, s, paddingDict[k], skip, netEmbedding)
+            network.append(MobileBottleNeck(inC, outC, e, k, s, paddingDict[k], skip, width))
+            inDim, outC = mobileBottleneckConv(inDim, inC, outC, e, k, s, paddingDict[k], skip, width, netEmbedding)
             numMB+=1
 
     ## X x 7 x 7
-    outChannelList = channelListPerDim[inDim]
+    outChannelList = channelListPerDim[size][inDim]
     inC = outC
     outC = random.choice(outChannelList)
     network.append(Conv2d(inC, outC, 1, 1, 0))
@@ -134,6 +156,12 @@ for i in range(numSamples):
 
     network.append(nn.MaxPool2d(7,7))
     inDim = pooling(inDim, 7, outC, netEmbedding)
+
+    outChannelList = channelListPerDim[size][inDim]
+    inC = outC
+    outC = random.choice(outChannelList)
+    network.append(Conv2d(inC, outC, 1, 1, 0))
+    inDim = convolution(inDim, inC, outC, 1, 1, 0, netEmbedding)
 
     inC = outC
     outC = 1000
@@ -154,28 +182,28 @@ for i in range(numSamples):
     print('Model ' + str(i) +' NumBottle:%d Skips: %d MACS: %f M   ModelSize: %f MB  Inference: %f ms' %(numMB, numSkip, macs/1e6, (params*4.0)/(1024**2), prof.self_cpu_time_total/1000.0))
     net.eval()
     
-    torch.onnx.export(net, x, "temp.onnx", export_params=True, opset_version=10, do_constant_folding=True, input_names=["input"], output_names=["output"],dynamic_axes={"input" : {0: "batch_size"},"output" : {0: "batch_size"}})
-    onnx_model = onnx.load("./temp.onnx")
-    onnx.checker.check_model(onnx_model)
-    inpt = ['input']
+    # torch.onnx.export(net, x, "temp.onnx", export_params=True, opset_version=10, do_constant_folding=True, input_names=["input"], output_names=["output"],dynamic_axes={"input" : {0: "batch_size"},"output" : {0: "batch_size"}})
+    # onnx_model = onnx.load("./temp.onnx")
+    # onnx.checker.check_model(onnx_model)
+    # inpt = ['input']
     
-    keras_model = onnx_to_keras(onnx_model=onnx_model, input_names=inpt, change_ordering=True, verbose=False)
-    converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
-    converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_LATENCY]
-    tflite_model = converter.convert()
-    open('model_latency/model_'+str(i)+'.tflite', "wb").write(tflite_model)
+    # keras_model = onnx_to_keras(onnx_model=onnx_model, input_names=inpt, change_ordering=True, verbose=False)
+    # converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
+    # converter.optimizations = [tf.lite.Optimize.OPTIMIZE_FOR_LATENCY]
+    # tflite_model = converter.convert()
+    # open('model_latency/model_'+str(i)+'.tflite', "wb").write(tflite_model)
     
-    data=''
-    for itr in netEmbedding:
-        for itr2 in itr:
-            data=data+str(itr2)+','
-    data=data[:-1]
-    data=data+'\n'
-    file.write(data)
+    # data=''
+    # for itr in netEmbedding:
+    #     for itr2 in itr:
+    #         data=data+str(itr2)+','
+    # data=data[:-1]
+    # data=data+'\n'
+    # file.write(data)
 # file.close()
-# plt.boxplot(flopsList)
-# plt.show()
-# plt.boxplot(modelSizeList)
-# plt.show()
-# plt.boxplot(inferenceTimeList)
-# plt.show()
+plt.boxplot(flopsList)
+plt.show()
+plt.boxplot(modelSizeList)
+plt.show()
+plt.boxplot(inferenceTimeList)
+plt.show()
