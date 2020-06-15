23005baa3acb0a96a7af0964fe79f0436d152d45
surya00060 <surya00060@gmail.com>
2020-06-15 23:51:16 +0530

    Added for mv1, proxyless. Need to finsh Squeeze

---
+++ MobileTargetedGenerator/existingmodelconvertion/genproxyembedd.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchprofile import profile_macs
+from mobilenetv1 import *
+#pip install timm
+import timm
+file = open("proxylessEmbeddings.csv", 'w')
+## Conv Attributes: in_channels': 256, 'out_channels': 256, 'kernel_size': (1, 1), 'stride': (1, 1), 'padding': (0, 0), 'dilation': (1, 1), 'transposed': False, 'output_padding': (0, 0), 'groups': 1,
+## FC Attributes: in_features': 256, 'out_features': 1000
+## ReLU Attributes: 
+def convolution(inDim, inC, outC, kernel, stride, padding, netEmbedding):
+    netEmbedding.append([1,0,0,0,0, inDim, inDim/stride, inC, outC, kernel, stride, padding, (inDim/stride)*(inDim/stride)*outC*inC*kernel*kernel*2])
+    inDim = inDim/stride
+    return inDim
+
+def depthConv(inDim, inC, kernel, stride, padding, netEmbedding):
+    netEmbedding.append([0,1,0,0,0, inDim, inDim/stride, inC, inC, kernel, stride, padding, (inDim/stride)*(inDim/stride)*inC*kernel*kernel*2])
+    inDim = inDim/stride
+    return inDim
+
+def relu(inDim, channels, netEmbedding):
+    netEmbedding.append([0,0,0,1,0, inDim, inDim, channels, channels, 0, 0, 0, inDim*inDim*channels])
+
+def pooling(inDim, kernel, channels, netEmbedding):
+    netEmbedding.append([0,0,0,0,1, inDim, inDim/kernel, channels, channels, 0, 0, 0, (inDim/kernel)*(inDim/kernel)*kernel*kernel*channels])
+    inDim = inDim/kernel
+    return inDim
+
+def skip(inDim, outChannels, netEmbedding):
+    netEmbedding.append([0,0,1,0,0, inDim, inDim, outChannels, outChannels, 0, 0, 0, inDim*inDim*outChannels])
+
+def generateEmbedding(model):
+    inDim = 224
+    netEmbedding = []
+    for name, module in model.named_modules():
+        if isinstance(module, nn.Conv2d):
+            if module.groups == 1:
+                inDim = convolution(inDim, module.in_channels, module.out_channels, module.kernel_size[0], module.stride[0], module.padding[0], netEmbedding)
+            else:
+                inDim = depthConv(inDim, module.in_channels, module.kernel_size[0], module.stride[0], module.padding[0], netEmbedding) 
+        
+        elif isinstance(module, nn.ReLU6) or isinstance(module, nn.ReLU):
+            if len(netEmbedding) == 0:
+                relu(inDim, 3, netEmbedding)
+            else:
+                channel = netEmbedding[-1][8]
+                relu(inDim, channel, netEmbedding)
+        
+        elif isinstance(module, nn.Linear):
+            inDim = convolution(inDim, module.in_features, module.out_features, 1, 1, 0, netEmbedding)
+        
+        elif isinstance(module, nn.AdaptiveAvgPool2d):
+            channel = netEmbedding[-1][8]
+            inDim = pooling(inDim, inDim, channel, netEmbedding)
+        
+        elif isinstance(module, nn.Identity):
+            channel = netEmbedding[-1][8]
+            skip(inDim, channel, netEmbedding)
+    
+    data=''
+    for itr in netEmbedding:
+        for itr2 in itr:
+            data=data+str(itr2)+','
+    data=data[:-1]
+    data=data+'\n'
+    file.write(data)
+
+x = torch.rand([1,3,224,224])
+# net = MobileNet(depth_mul=0.25)
+# generateEmbedding(net)
+# net = MobileNet(depth_mul=0.5)
+# generateEmbedding(net)
+# net = MobileNet(depth_mul=0.75)
+# generateEmbedding(net)
+# net = MobileNet(depth_mul=1.0)
+# generateEmbedding(net)
+# net = torchvision.models.mobilenet_v2(width_mult=1)
+# net = torch.jit.trace(net, x)
+# print(net)
+
+# generateEmbedding(net)
+target_platform = "proxyless_mobile"
+net = torch.hub.load('mit-han-lab/ProxylessNAS', target_platform, pretrained=False)
+generateEmbedding(net)
+# net = timm.create_model('fbnetc_100', pretrained=False)
+# net = timm.create_model('spnasnet_100', pretrained=False)
+target_platform = "proxyless_mobile_14"
+net = torch.hub.load('mit-han-lab/ProxylessNAS', target_platform, pretrained=False)
+generateEmbedding(net)
+# print(net)
+# net = torchvision.models.mnasnet0_5()
+# net = torchvision.models.mnasnet0_75()
+# net = torchvision.models.mnasnet1_0()
+# net = torchvision.models.mnasnet1_3()
+# net = torchvision.models.squeezenet1_0()
+# net = torchvision.models.squeezenet1_1()
+# net = torchvision.models.mobilenet_v2(width_mult=0.75)
+# net = torchvision.models.mobilenet_v2(width_mult=0.5)
+# net = torchvision.models.mobilenet_v2(width_mult=0.25)
+# net = efn.EfficientNetB0(weights='imagenet')
+# net = efn.EfficientNetB1(weights='imagenet')
+# net = efn.EfficientNetB2(weights='imagenet')
+# net = efn.EfficientNetB3(weights='imagenet')
+file.close()
--- MobileTargetedGenerator/existingmodelconvertion/mobilenetv1.py
+++ MobileTargetedGenerator/existingmodelconvertion/mobilenetv1.py
@@ -16,58 +16,11 @@ class Block(nn.Module):
         self.bn1 = nn.BatchNorm2d(in_planes)
         self.conv2 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn2 = nn.BatchNorm2d(out_planes)
-
+        self.relu = nn.ReLU(inplace=True)
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn2(self.conv2(out)))
         return out
-
-class FriendlyBlock(nn.Module):
-    '''Depthwise conv + Pointwise conv'''
-    def __init__(self, in_planes, out_planes, stride=1):
-        super(FriendlyBlock, self).__init__()
-        self.conv1_h = nn.Conv2d(in_planes, in_planes, kernel_size=(1,3), stride=stride, padding=(0,1), groups=in_planes, bias=False)
-        self.conv1_v = nn.Conv2d(in_planes, in_planes, kernel_size=(3,1), stride=stride, padding=(1,0), groups=in_planes, bias=False)
-        self.bn1 = nn.BatchNorm2d(2*in_planes)
-        self.conv2 = nn.Conv2d(2*in_planes, out_planes, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_planes)
-
-    def forward(self, x):
-        out1 = self.conv1_h(x)
-        out2 = self.conv1_v(x)
-        out  = torch.cat((out1, out2), 1)
-        out = self.bn1(out)
-        out = F.relu(self.bn2(self.conv2(out)))
-        return out
-
-class MobileNet(nn.Module):
-    # (128,2) means conv planes=128, conv stride=2, by default conv stride=1
-    cfg = [64, (128,2), 128, (256,2), 256, (512,2), 512, 512, 512, 512, 512, (1024,2), 1024]
-
-    def __init__(self, num_classes=1000):
-        super(MobileNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.layers = self._make_layers(in_planes=32)
-        self.linear = nn.Linear(1024, num_classes)
-
-    def _make_layers(self, in_planes):
-        layers = []
-        for x in self.cfg:
-            out_planes = x if isinstance(x, int) else x[0]
-            stride = 1 if isinstance(x, int) else x[1]
-            layers.append(Block(in_planes, out_planes, stride))
-            in_planes = out_planes
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layers(out)
-        out = F.avg_pool2d(out, 7)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-
 
 class MobileNet(nn.Module):
     # (128,2) means conv planes=128, conv stride=2, by default conv stride=1
@@ -80,7 +33,8 @@ class MobileNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(32)
         self.layers = self._make_layers(in_planes=32)
         self.linear = nn.Linear(int(1024 * self.depth_mul), num_classes)
-
+        self.relu = nn.ReLU(inplace=True)
+        self.avgpool = nn.AvgPool2d(7)
     def _make_layers(self, in_planes):
         layers = []
         for x in self.cfg:
@@ -91,10 +45,9 @@ class MobileNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn1(self.conv1(x)))
         out = self.layers(out)
-        out = F.avg_pool2d(out, 7, stride=7)
-        #out = out.view(out.size(0), -1)
+        out = self.avgpool2d(out)
         out = torch.flatten(out, start_dim=1)
         out = self.linear(out)
         return out
