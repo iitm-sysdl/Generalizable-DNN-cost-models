c2461ca06f6bf1d8db318080d75cbff69ab95b5b
surya00060 <surya00060@gmail.com>
2020-06-18 11:06:47 +0530

    Changed 10 to 7

--- LSTMModels/mobile_data/clusterscript.sh
+++ LSTMModels/mobile_data/clusterscript.sh
@@ -3,6 +3,6 @@ for i in random mutual_info_v2 spearmanCorr
 do 
     for j in slow medium fast
     do
-        python cluster.py --sampling_type=$i --learning_type=combined --name=transfer$j$i --numSamples=10 --model=xgb --transfer=$j
+        python cluster.py --sampling_type=$i --learning_type=combined --name=transfer$j$i --numSamples=7 --model=xgb --transfer=$j
     done
 done
