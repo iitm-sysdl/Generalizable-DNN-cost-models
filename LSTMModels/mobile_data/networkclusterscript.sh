e6984754da3eca5e9237fe8b42e6a8776e83daad
surya00060 <surya00060@gmail.com>
2020-06-18 11:31:58 +0530

    Added Selecting features from same network cluster

---
+++ LSTMModels/mobile_data/networkclusterscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for i in random mutual_info_v2 spearmanCorr
+do 
+    for j in small large giant
+    do
+        for k in 3 5 7
+        do
+            cluster="cluster"
+            samples="samples"
+            cluster="cluster"
+            var="$k$samples$j$cluster$i"
+            python networkcluster.py --sampling_type=$i --learning_type=combined --name=$var --numSamples=$k --model=xgb --networkcluster=$j
+        done
+    done
+done
